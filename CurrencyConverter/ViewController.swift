07efdcc03bc3403f4eedafcfa47d523dcbac550e
Burak <karagulburak97@gmail.com>
2022-04-08 01:20:41 +0300

    Created

---
+++ CurrencyConverter/ViewController.swift
@@ -0,0 +1,104 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Burak Karagül on 23.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var cadLabel: UILabel!
+    @IBOutlet weak var chfLabel: UILabel!
+    @IBOutlet weak var gbpLabel: UILabel!
+    @IBOutlet weak var jpyLabel: UILabel!
+    @IBOutlet weak var usdLabel: UILabel!
+    @IBOutlet weak var tryLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func getRatesClicked(_ sender: Any) {
+        
+//        1) Request & Session
+//        2) Response & Data
+//        3) Parsing & Json Serialization
+        
+        let url = URL(string: "http://data.fixer.io/api/latest?access_key=c1b031a784410a14a1f975e2226da70a&format=1")
+        
+        let session = URLSession.shared
+        
+        
+        let task = session.dataTask(with: url!) { data, response, error in
+            
+            
+            if error != nil{
+                
+                
+                let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                
+                
+                let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+                alert.addAction(okButton)
+                
+                
+                self.present(alert, animated: true, completion: nil)
+                
+            }else{
+
+                
+                if data != nil {
+                
+                    
+                    do{
+                        let jsonResponse = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as! Dictionary<String, Any>
+                        
+                        
+                        DispatchQueue.main.async {
+                            
+                            
+                            if let rates = jsonResponse["rates"] as? [String : Any]{
+                                if let cad = rates["CAD"] as? Double {
+                                    self.cadLabel.text="CAD: \(cad)" //Zaten tırnak içi olduğu için string(cad) yazmam gerek kalmadan cast ediyor.
+                                }
+                                if let chf = rates["CHF"] as? Double {
+                                    self.chfLabel.text="CHF: \(chf)" //Zaten tırnak içi olduğu için string(cad) yazmam gerek kalmadan cast ediyor.
+                                }
+                                if let gbp = rates["GBP"] as? Double {
+                                    self.gbpLabel.text="GBP: \(gbp)" //Zaten tırnak içi olduğu için string(cad) yazmam gerek kalmadan cast ediyor.
+                                }
+                                if let jpy = rates["JPY"] as? Double {
+                                    self.jpyLabel.text="JPY: \(jpy)" //Zaten tırnak içi olduğu için string(cad) yazmam gerek kalmadan cast ediyor.
+                                }
+                                if let usd = rates["USD"] as? Double {
+                                    self.usdLabel.text="USD: \(usd)" //Zaten tırnak içi olduğu için string(cad) yazmam gerek kalmadan cast ediyor.
+                                }
+                                if let tr = rates["TRY"] as? Double {
+                                    self.tryLabel.text="TRY: \(tr)" //Zaten tırnak içi olduğu için string(cad) yazmam gerek kalmadan cast ediyor.
+                                }
+                            }
+                        }
+                        
+                    }catch{
+                        print(error.localizedDescription)
+                    }
+                    
+                    
+                }
+                
+                
+            }
+        }
+        
+        
+        task.resume()
+        
+    }
+    
+
+}
+
